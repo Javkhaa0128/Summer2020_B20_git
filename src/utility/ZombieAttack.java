cc4e90b80b86aa1b4eb801ea85d9d77f508831c3
Javkhaa0128 <javkhaa0128@gmail.com>
2020-08-06 14:50:25 -0400

    My name

---
+++ src/utility/ZombieAttack.java
@@ -0,0 +1,14 @@
+package utility;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ZombieAttack {
+
+        public static void main(String[] args) {
+
+            // needs code
+            System.out.println("Javkhlantugs");
+        }
+    }
+
